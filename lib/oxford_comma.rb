e1206d16cb2a245e1460ca09ae2916f4a8e03129
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-04 23:49:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
  elsif array.length == 1
   array[0]
 else array.length > 2
-  "#{array[0..-1].join(", ")} and #{array[0]}"
+  "#{array[0..-3].join(", ")} and #{array.last}"
 end
 end
