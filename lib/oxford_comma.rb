4ba0f6d10d9e16dadcee81d788d34c4d9ff5bf8c
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-05 00:21:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
  elsif array.length == 1
   array[0]
 else array.length >= 3
-  [array[0...-1].join(", "), array.last].join(", and ")
+  [array[0...-1].join(", "), array.last].join(", and , ")
 end
 end
